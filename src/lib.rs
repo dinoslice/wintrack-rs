8cc6db097dff7ea5415794795f73d31dca429cd5
abscosmos <joshua@dinoslice.com>
2025-06-10 09:40:19 -0500

    Get window title in hooked function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+use std::ffi::OsString;
+use std::os::windows::ffi::OsStringExt;
 use windows::Win32::Foundation::HWND;
-use windows::Win32::UI::Accessibility::HWINEVENTHOOK;
-use windows::Win32::UI::WindowsAndMessaging::{CHILDID_SELF, EVENT_OBJECT_NAMECHANGE, OBJID_WINDOW};
+use windows::Win32::UI::Accessibility::{HWINEVENTHOOK};
+use windows::Win32::UI::WindowsAndMessaging::{GetWindowTextLengthW, GetWindowTextW, CHILDID_SELF, EVENT_OBJECT_NAMECHANGE, OBJID_WINDOW};
 
 unsafe extern "system" fn win_event_proc(
     _h_win_event_hook: HWINEVENTHOOK,
@@ -12,6 +14,27 @@ unsafe extern "system" fn win_event_proc(
     _dwms_event_time: u32,
 ) {
     if event == EVENT_OBJECT_NAMECHANGE && id_object == OBJID_WINDOW.0 && id_child == CHILDID_SELF as _ {
-        println!("title changed!")
+        if let Some(title) = unsafe { get_window_title(hwnd) } {
+            if !title.is_empty() {
+                println!("Window title changed: \"{}\"", title);
+            }
+        }
     }
+}
+
+unsafe fn get_window_title(hwnd: HWND) -> Option<String> {
+    let length = unsafe { GetWindowTextLengthW(hwnd) };
+    if length == 0 {
+        return None;
+    }
+
+    let mut buffer = vec![0; (length + 1) as usize];
+
+    let copied = unsafe { GetWindowTextW(hwnd, &mut buffer) };
+    if copied == 0 {
+        return None;
+    }
+
+    let os_string = OsString::from_wide(&buffer[..copied as usize]);
+    os_string.into_string().ok()
 }
