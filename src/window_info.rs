346e93e0818f4c93e94c0923eb6fe62966e5bce5
abscosmos <joshua@dinoslice.com>
2025-06-10 17:29:16 -0500

    Create function to get class name of a window

--- src/window_info.rs
+++ src/window_info.rs
@@ -2,7 +2,7 @@ use std::ffi::OsString;
 use std::os::windows::ffi::OsStringExt;
 use windows::Win32::Foundation::{GetLastError, SetLastError, ERROR_SUCCESS, HWND};
 use windows::core::Error as WinErr; 
-use windows::Win32::UI::WindowsAndMessaging::{GetWindowTextLengthW, GetWindowTextW};
+use windows::Win32::UI::WindowsAndMessaging::{GetClassNameW, GetWindowTextLengthW, GetWindowTextW};
 
 fn get_window_title(hwnd: HWND) -> Result<String, WinErr> {
     // clear last error to ensure GetWindowTextLengthW result can be used
@@ -29,5 +29,14 @@ fn get_window_title(hwnd: HWND) -> Result<String, WinErr> {
 }
 
 fn get_window_class_name(hwnd: HWND) -> Result<String, WinErr> {
+    // https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-wndclassa
+    // "The maximum length for lpszClassName is 256 ..."
+    const CLASS_NAME_MAX_LEN: usize = 256;
     
+    let mut buffer = [0u16; CLASS_NAME_MAX_LEN];
+
+    match unsafe { GetClassNameW(hwnd, &mut buffer) } {
+        0 => Err(WinErr::from_win32()),
+        copied => Ok(String::from_utf16_lossy(&buffer[..copied as usize]))
+    }
 }
