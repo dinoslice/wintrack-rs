1736e9de9cee14bc60e006f3bdb53949155c5c32
abscosmos <joshua@dinoslice.com>
2025-06-10 21:31:45 -0500

    Get window integrity level

--- src/window_info.rs
+++ src/window_info.rs
@@ -2,9 +2,10 @@ use std::ffi::OsString;
 use std::num::NonZeroU32;
 use std::os::windows::ffi::OsStringExt;
 use std::path::PathBuf;
-use windows::Win32::Foundation::{CloseHandle, SetLastError, ERROR_INVALID_WINDOW_HANDLE, ERROR_SUCCESS, HWND, MAX_PATH, RECT};
+use windows::Win32::Foundation::{CloseHandle, SetLastError, ERROR_INSUFFICIENT_BUFFER, ERROR_INVALID_WINDOW_HANDLE, ERROR_SUCCESS, HANDLE, HWND, MAX_PATH, RECT};
 use windows::core::{Error as WinErr, PWSTR};
-use windows::Win32::System::Threading::{OpenProcess, QueryFullProcessImageNameW, PROCESS_NAME_NATIVE, PROCESS_NAME_WIN32, PROCESS_QUERY_LIMITED_INFORMATION};
+use windows::Win32::Security::{GetSidSubAuthority, GetSidSubAuthorityCount, GetTokenInformation, TokenIntegrityLevel, TOKEN_MANDATORY_LABEL, TOKEN_QUERY};
+use windows::Win32::System::Threading::{OpenProcess, OpenProcessToken, QueryFullProcessImageNameW, PROCESS_NAME_WIN32, PROCESS_QUERY_LIMITED_INFORMATION};
 use windows::Win32::UI::WindowsAndMessaging::{GetClassNameW, GetForegroundWindow, GetWindowRect, GetWindowTextLengthW, GetWindowTextW, GetWindowThreadProcessId, IsIconic, IsWindow, IsWindowVisible};
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
@@ -17,13 +18,16 @@ pub struct WindowSnapshot {
     pub is_minimized: bool,
     pub is_foreground: bool,
     pub executable: PathBuf,
+    pub integrity_level: IntegrityLevel,
 }
 
 impl WindowSnapshot {
     pub fn from_hwnd(hwnd: HWND) -> Result<Self, WinErr> {
         let (thread_id, process_id) = get_window_thread_process_id(hwnd)?;
         
-        Ok(Self {
+        let process_handle = unsafe { OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, false, process_id)? };
+        
+        let snapshot = Self {
             title: get_window_title(hwnd)?,
             class_name: get_window_class_name(hwnd)?,
             rect: get_window_rect(hwnd)?,
@@ -31,8 +35,13 @@ impl WindowSnapshot {
             process_id,
             is_minimized: is_window_minimized(hwnd).ok_or(WinErr::from(ERROR_INVALID_WINDOW_HANDLE))?,
             is_foreground: is_window_foreground(hwnd).ok_or(WinErr::from(ERROR_INVALID_WINDOW_HANDLE))?,
-            executable: get_process_executable_path(process_id)?,
-        })
+            executable: get_process_executable_path(process_handle)?,
+            integrity_level: get_process_integrity_level(process_handle)?,
+        };
+
+        unsafe { CloseHandle(process_handle)?; }
+        
+        Ok(snapshot)
     }
 }
 
@@ -144,17 +153,80 @@ fn get_window_thread_process_id(hwnd: HWND) -> Result<(WinThreadId, WinProcessId
     }
 }
 
-fn get_process_executable_path(pid: WinProcessId) -> Result<PathBuf, WinErr> {
-    let process_handle = unsafe { OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, false, pid)? };
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Hash)]
+pub enum IntegrityLevel {
+    Low,
+    Medium,
+    MediumUiAccess,
+    High,
+    System,
+    Protected,
+    Other(u32),
+}
 
+impl IntegrityLevel {
+    pub fn from_rid(rid: u32) -> Self {
+        match rid {
+            0x1000 => IntegrityLevel::Low,
+            0x2000 => IntegrityLevel::Medium,
+            0x2100 => IntegrityLevel::MediumUiAccess,
+            0x3000 => IntegrityLevel::High,
+            0x4000 => IntegrityLevel::System,
+            0x5000 => IntegrityLevel::Protected,
+            other => IntegrityLevel::Other(other),
+        }
+    }
+
+    pub fn rid(&self) -> u32 {
+        match *self {
+            IntegrityLevel::Low => 0x1000,
+            IntegrityLevel::Medium => 0x2000,
+            IntegrityLevel::MediumUiAccess => 0x2100,
+            IntegrityLevel::High => 0x3000,
+            IntegrityLevel::System => 0x4000,
+            IntegrityLevel::Protected => 0x5000,
+            IntegrityLevel::Other(value) => value,
+        }
+    }
+}
+
+fn get_process_integrity_level(process_handle: HANDLE) -> Result<IntegrityLevel, WinErr> {
+    let mut token_handle = HANDLE::default();
+    
+    unsafe { OpenProcessToken(process_handle, TOKEN_QUERY, &mut token_handle)? };
+    
+    let mut ret_len = 0;
+    
+    match unsafe { GetTokenInformation(token_handle, TokenIntegrityLevel, None, 0, &mut ret_len) } {
+        Err(err) if err != ERROR_INSUFFICIENT_BUFFER.into() => {
+            unsafe { CloseHandle(token_handle)?; }
+            
+            return Err(err);
+        },
+        _ => {},
+    }
+    
+    let mut buffer = vec![0u8; ret_len as _];
+
+    unsafe { GetTokenInformation(token_handle, TokenIntegrityLevel, Some(buffer.as_mut_ptr() as _), ret_len, &mut ret_len)? };
+    
+    let security_id = unsafe { *buffer.as_ptr().cast::<TOKEN_MANDATORY_LABEL>() }.Label.Sid;
+
+    let count = unsafe { *GetSidSubAuthorityCount(security_id) } as u32;
+    let rid = unsafe { *GetSidSubAuthority(security_id, count - 1) };
+
+    unsafe { CloseHandle(token_handle)?; }
+    
+    Ok(IntegrityLevel::from_rid(rid))
+}
+
+fn get_process_executable_path(process_handle: HANDLE) -> Result<PathBuf, WinErr> {
     let mut buffer = [0u16; MAX_PATH as _];
 
     let mut chars = buffer.len() as _;
 
     unsafe { QueryFullProcessImageNameW(process_handle, PROCESS_NAME_WIN32, PWSTR(buffer.as_mut_ptr()), &mut chars)? };
-
-    unsafe { CloseHandle(process_handle)? };
-
+    
     Ok(PathBuf::from(OsString::from_wide(&buffer[..chars as _])))
 }
 
